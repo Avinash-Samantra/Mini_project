a49b38880d671dbaca45d6b201b91560fd51f9ad
Avinash-Samantra <avinash02175@gmail.com>
2025-04-16 23:43:08 +0530

    commited

--- backend/server.js
+++ backend/server.js
@@ -21,8 +21,11 @@ app.use("/api/products", productRoutes);
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	// app.get("*", (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	//   });
+	app.get("/:path(.*)", (req, res) => {
+		res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 	  });
 }
 
